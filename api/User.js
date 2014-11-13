716e6017f69fff31bbae364473824489a96490ee
Durgesh Kumar <durgesh.kumar@linkites.com>
2014-11-13 17:25:29 +0530

    first commit

---
+++ api/User.js
@@ -0,0 +1,113 @@
+var ObjectID = require('mongodb').ObjectID;
+var assert = require('assert');
+var BSON = require('mongodb').BSONPure;
+var monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+module.exports = { 
+    name: "User",
+	checkInHistory : function(req, res, next){
+		var userID = req.params.userID;
+		var db = req.db;
+
+		db.collection("checkIns").aggregate({$group : { _id : "$time", checkIn: { $push: "$$ROOT" } } },
+		{ $sort : { _id : -1} } , function(err, data){
+
+			var i = -1;
+			function nextGroup(){
+				i++;
+				if(i<data.length){
+					var j = -1;
+					
+					function nextCheckInHistory(){
+						j++;
+						if(data[i].checkIn.length > j){
+							formateDate(data[i].checkIn[j].time, function(convdt){
+								data[i].checkIn[j].time = convdt;
+								data[i].date = convdt;
+								delete data[i]._id;
+								var isNext = true;
+								if(data[i].checkIn[j].checkin_type == "r") data[i].checkIn[j].points = -(data[i].checkIn[j].points);
+								if(data[i].checkIn[j].vendor_id != null) {
+									isNext = false;
+									findvendor(data[i].checkIn[j].vendor_id, function(err, vendor){
+										if(err){
+											console.log(err);
+										} else if(vendor) {
+											data[i].checkIn[j].vendor_logo = vendor.vendor_logo;
+										}
+										nextCheckInHistory();
+									});
+								}
+								if(isNext) nextCheckInHistory();
+							});
+								
+						} else {
+							nextGroup();
+						}
+					}
+					nextCheckInHistory();
+				} else {
+					res.send(data);
+				}
+			}
+			nextGroup();
+			function findvendor(vendor_id, callback){
+			db.collection("vendorsList").findOne({vendor_id : vendor_id}, function(err, vendor){
+				if(err){
+					callback(err);
+				} else {
+					callback(null, vendor);
+				}
+			});
+			}
+
+			function formateDate(dtstr, callback){
+				dtstr = dtstr.replace(/\D/g," ");
+				var dtcomps = dtstr.split(" ");
+				dtcomps[1]--;
+				var convdt = new Date(Date.UTC(dtcomps[0],dtcomps[1],dtcomps[2],dtcomps[3],dtcomps[4],dtcomps[5]));
+				convdt = monthArray[convdt.getMonth()] + " " + (convdt.getDay()>9?convdt.getDay():("0"+convdt.getDay())) + ", " + convdt.getFullYear();
+				callback(convdt);
+			}
+		});
+		
+		// db.collection("checkIns").find({"userId": userID}).sort({"time":-1}).toArray(function(err, user){
+			
+		// 	if(err) res.json({error:"Error"});
+		// 	else {
+		// 		var i = -1;
+		// 		function nextCheckInHistory(){
+		// 			i++;
+		// 			if(user.length > i){
+		// 				formateDate(user[i].time, function(convdt){
+		// 					user[i].time = convdt;
+		// 					var isNext = true;
+		// 					if(user[i].checkin_type == "r") user[i].points = -(user[i].points);
+		// 					if(user[i].vendor_id != null) {
+		// 						isNext = false;
+		// 						findvendor(user[i].vendor_id, function(err, vendor){
+		// 							if(err){
+		// 								console.log(err);
+		// 							} else if(vendor) {
+		// 								user[i].vendor_logo = vendor.vendor_logo;
+		// 							}
+		// 							nextCheckInHistory();
+		// 						});
+		// 					}
+		// 					if(isNext) nextCheckInHistory();
+		// 				});
+							
+		// 			} else {
+		// 				res.json(user);
+		// 			}
+		// 		}
+		// 		//nextCheckInHistory();
+
+				
+		// 	}
+		// });
+
+		
+
+	}
+};
---
+++ app.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var debug = require('debug')('apiproject');
+
+
+var User = require('./api/User')
+
+var app = express();
+
+
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'ABCOWFWEFWKDC237634274CHMCJGSDF',resave: true,
+    saveUninitialized: true}))
+
+var db;
+var url = 'mongodb://107.170.72.89:29065/Pricehowl';
+//Use connect method to connect to the Server
+MongoClient.connect(url, function(err, database) {
+    assert.equal(null, err);
+    if(err) console.log(err);
+    else{
+      db=database;
+    }
+});
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+
+app.use('/user/:userID/chekinHistory', function(req, res, next){
+    User.checkInHistory(req, res, next);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({message : "Not found"});
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+
+
+module.exports = app;
